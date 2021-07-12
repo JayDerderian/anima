08aaabf6f1619905b4445979be13f60bf5e9dae1
jay <jayd@pdx.edu>
2021-07-11 19:16:20 -0700

    added some new fields to composition()

--- containers/composition.py
+++ containers/composition.py
@@ -1,17 +1,25 @@
-#*******************************************************************************************#
-#-------------------------------This class composition data---------------------------------#
-#*******************************************************************************************#
+#***************************************************************************************************#
+#-------------------------------This class handles composition data---------------------------------#
+#***************************************************************************************************#
 
 class composition():
     '''
     This is a container for all things related to a stand-alone music composition. Data includes
-    a global tempo, the piece's title, an array of melody() objects, and an array of chord() objects.
+    a global tempo, the piece's title, an array of melody() objects representing each instruments part, 
+    an array of chord() objects for harmony instruments, and other pretinant data about the piece.
+
+    Title, composer, date of composition, file name, source data (if any), global tempo,
+    instrument list, melodies, and harmonies.
     '''
     # Constructor
     def __init__(self):
 
         # Title
         self.title = ""
+        # Composer
+        self.composer = ""
+        # Date of composition
+        self.date = ""
         # File name for associated .txt file. May need to store
         # individual user's file path here?
         self.fileName = ""
@@ -25,6 +33,10 @@ class composition():
         '''
         # Global tempo (float)
         self.tempo = 0.0
+        '''
+        NOTE: instrument and melody lists need to coinicide so that the MIDI
+        file can select the proper instruments. 
+        '''
         # List of instruments (strings)
         self.instruments = []
         # List of melody() objects. 
@@ -45,6 +57,8 @@ class composition():
         -List of harmonies (chord() objects)
         '''
         if(self.title != "" and
+            self.composer != "" and
+            self.date != "" and
             self.fileName != "" and
             self.tempo != 0.0 and
             len(self.sourceData) != 0 and
@@ -55,6 +69,10 @@ class composition():
         else:
             if(self.title == ""):
                 print("\ncomposition() - ERROR: no title inputted!")
+            elif(self.composer == ""):
+                print("\ncomposition() - ERROR: no composer info inputted!")
+            elif(self.date == ""):
+                print("\ncomposition() - ERROR: no date info inputted!")
             elif(self.fileName == ""):
                 print("\ncomposition() - ERROR: no file name inputted!")
             elif(self.tempo == 0.0):
