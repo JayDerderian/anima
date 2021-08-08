0b6b01dea3d140cff8909d2bf3ed7717e693b936
jay <maestro1286@gmail.com>
2021-08-08 10:05:18 -0700

    moved test.py again

--- modes/rando.py
+++ modes/rando.py
@@ -7,8 +7,8 @@ may vary substantially, as well as the instrumentation.
 #IMPORTS
 import midi as m
 import constants as c
-from test import newData
 from random import randint
+from utils.test import newData
 from datetime import datetime as date
 from generate import generate as create
 from containers.composition import composition
