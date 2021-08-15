01f0834f8a8fe6237a42c74f56c602caeb1011b5
jay <maestro1286@gmail.com>
2021-08-15 11:01:43 -0700

    misc

--- core/generate.py
+++ core/generate.py
@@ -5,9 +5,6 @@ This module/class handles all generative methods.
 '''
 ----------------------------------------------------NOTES-------------------------------------------------------
 
-    TODO: Import modes and use Generate() to call them? Seems it'll be easier than trying to 
-          import Generate() to any of the modes and utilizing the modules on their own.
-
 
     GENERAL NOTES:
 
@@ -508,69 +505,6 @@ class Generate():
         row = self.toStr(pcs, octave=4)
         return row, pcs
 
-    # Generates a 12-tone matrix from a given row
-    def newMatrix(self, row, intrvls):
-        '''
-        NOTE: NOT READY
-
-        Generates a 2-D array/12-tone matrix from a given pitch class set (pcs = list[int]). 
-        Requires a list of 11 positive intervals between 1-11 ([1, 4, 2, 6]) to iterate off of.
-
-        The matrix is generating by appending a transposition
-        of the original row to each subsequent index. 
-        All other information, such as retrogressions, inversions, and 
-        retrogressions + inversions can found using some print tricks.
-
-        Returns a 2-D matrix - 'm'
-
-        ---------
-
-        Print original row:        
-            print(m[0])
-    
-        Print each row retrograde:
-            for i in range(len(m[i])):
-                retro = m[i]
-                retro.reverse()
-                print(retro)
-
-        Print each row inversions (matrix column, top to bottom):
-            for i in range(len(m))
-                inv = [row[i] for row in m]
-                print(inv)
-
-        Print each row retrograde inversions (matrix column, bottom to top):
-            for i in range(len(m))
-                ret_inv = [row[i] for row in m]
-                ret_inv[i].reverse()
-                print(ret_inv)
-
-        --------
-        NOTE: maybe there's a way to poplulate the matrix using synxtax like this:
-        arr = [[r]*cols]*rows, where r is a modified version (transposition) of the 
-        original row. 
-        
-        rows and cols are declared as a tuple (rows, cols = (n, n) 
-        where n is some int)
-        '''
-        m = [[]]
-        # add original row to first matrix row
-        m.append(row)
-        for i in range(len(intrvls)):
-            r = self.transpose(row, intrvls[i])
-            print("\nadding P", intrvls[i], ":", r)
-            m.insert(i, r)
-        return m
-
-    # display 12-tone matrix
-    def printMatrix(self, matrix):
-        '''
-        Display a twelve-tone matrix (2D list)
-        '''
-        for x in matrix:
-            for y in x:
-                print(y, end = " ")
-            print()
 
     # generate a list of 11 intervals to transpose a 12-tone row by to generate
     # a matrix
