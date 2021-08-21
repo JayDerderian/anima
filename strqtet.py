bebac4dea70905e956bbad473e6d626d9bf49681
jay <maestro1286@gmail.com>
2021-08-21 08:08:49 -0700

    fixed a bug in newStrQtet()

--- strqtet.py
+++ strqtet.py
@@ -18,10 +18,10 @@ def newStrQtet():
     # objects
     create = Generate()
     comp = Composition()
-    v1_melody = Melody()
-    v2_melody = Melody()
-    va_melody = Melody()
-    vc_melody = Melody()
+    v1 = Melody()
+    v2 = Melody()
+    va = Melody()
+    vc = Melody()
 
     # title 'n stuff
     comp.title = create.newTitle()
@@ -31,20 +31,20 @@ def newStrQtet():
     comp.date = date.now().strftime("%d-%b-%y %H:%M:%S")
 
     # disperse tempo
-    v1_melody.tempo = comp.tempo
-    v2_melody.tempo = comp.tempo
-    va_melody.tempo = comp.tempo
-    vc_melody.tempo = comp.tempo
+    v1.tempo = comp.tempo
+    v2.tempo = comp.tempo
+    va.tempo = comp.tempo
+    vc.tempo = comp.tempo
 
     # assign instruments
-    v1_melody.instrument = "Violin"
-    comp.instruments.append(v1_melody.instrument)
-    v2_melody.instrument = "Violin"
-    comp.instruments.append(v2_melody.instrument)
-    va_melody.instrument = "Viola"
-    comp.instruments.append(va_melody.instrument)
-    vc_melody.instrument = "Cello"
-    comp.instruments.append(vc_melody.instrument)
+    v1.instrument = "Violin"
+    comp.instruments.append(v1.instrument)
+    v2.instrument = "Violin"
+    comp.instruments.append(v2.instrument)
+    va.instrument = "Viola"
+    comp.instruments.append(va.instrument)
+    vc.instrument = "Cello"
+    comp.instruments.append(vc.instrument)
 
     # working with pan-diatonic music for now...
     root = c.MAJOR_SCALES[randint(1, len(c.MAJOR_SCALES) - 1)]
@@ -68,6 +68,7 @@ def newStrQtet():
                 octave += 1
                 n = 0
         scales[i] = scale
+        root = c.MAJOR_SCALES[randint(1, len(c.MAJOR_SCALES) - 1)]
 
     # generate parts. each will have a different amount of notes, hence the multiple loops. 
     # unfortunately this method doesn't allow for immediate repetition of notes (unless by chance). 
@@ -79,18 +80,18 @@ def newStrQtet():
         total = randint(25, 50)
         for j in range(total):
             # limited to octaves 4 and 5
-            v1_melody.notes.append(scale[randint(7, len(scale) - 1)])
-    v1_melody.rhythms = create.newRhythms(total=len(v1_melody.notes), tempo=comp.tempo)
-    v1_melody.dynamics = create.newDynamics(total=len(v1_melody.notes))
+            v1.notes.append(scale[randint(7, len(scale) - 1)])
+    v1.rhythms = create.newRhythms(total=len(v1.notes), tempo=comp.tempo)
+    v1.dynamics = create.newDynamics(total=len(v1.notes))
     print("\nwriting violin 2 part...")
     for i in range(len(scales)):
         scale = scales[i]
         total = randint(25, 50)
         for j in range(total):
             # limited to octaves 4 and 5
-            v2_melody.notes.append(scale[randint(7, len(scale) - 1)])
-    v2_melody.rhythms = create.newRhythms(total=len(v2_melody.notes), tempo=comp.tempo)
-    v2_melody.dynamics = create.newDynamics(total=len(v2_melody.notes))
+            v2.notes.append(scale[randint(7, len(scale) - 1)])
+    v2.rhythms = create.newRhythms(total=len(v2.notes), tempo=comp.tempo)
+    v2.dynamics = create.newDynamics(total=len(v2.notes))
 
     # viola part
     print("\nwriting viola part...")
@@ -99,9 +100,9 @@ def newStrQtet():
         total = randint(25, 50)
         for j in range(total):
             # limited to octaves 3 and 4
-            va_melody.notes.append(scale[randint(7, len(scale) - 8)])
-    va_melody.rhythms = create.newRhythms(total=len(va_melody.notes), tempo=comp.tempo)
-    va_melody.dynamics = create.newDynamics(total=len(va_melody.notes))
+            va.notes.append(scale[randint(7, len(scale) - 8)])
+    va.rhythms = create.newRhythms(total=len(va.notes), tempo=comp.tempo)
+    va.dynamics = create.newDynamics(total=len(va.notes))
 
     # cello part
     print("\nwriting cello part...")
@@ -110,15 +111,15 @@ def newStrQtet():
         total = randint(25, 50)
         for j in range(total):
             # limited to octaves 2 - 3
-            vc_melody.notes.append(scale[randint(0, len(scale) - 16)])
-    vc_melody.rhythms = create.newRhythms(total=len(vc_melody.notes), tempo=comp.tempo)
-    vc_melody.dynamics = create.newDynamics(total=len(vc_melody.notes))
+            vc.notes.append(scale[randint(0, len(scale) - 16)])
+    vc.rhythms = create.newRhythms(total=len(vc.notes), tempo=comp.tempo)
+    vc.dynamics = create.newDynamics(total=len(vc.notes))
 
     # save all parts and write out
-    comp.melodies.append(v1_melody)
-    comp.melodies.append(v2_melody)
-    comp.melodies.append(va_melody)
-    comp.melodies.append(vc_melody)
+    comp.melodies.append(v1)
+    comp.melodies.append(v2)
+    comp.melodies.append(va)
+    comp.melodies.append(vc)
 
     # generate MIDI and .txt file names
     print("\ngenerating file names...")
@@ -129,8 +130,7 @@ def newStrQtet():
     title_full = "{}{}".format(comp.title, ' for string quartet')
 
     # write to MIDI file and .txt file
-    '''NOTE: saves in "minimal" style!'''
-    if saveMinimal(comp) == 0:
+    if save(comp) == 0:
         saveInfo(name=comp.title, fileName=comp.txtFileName, newMusic=comp)
         print("\n\nnew quartet:", title_full)
         print("\ncomposer:", comp.composer)
